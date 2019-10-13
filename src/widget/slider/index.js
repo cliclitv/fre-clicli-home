0bdc7687762261459139b5743c66d5783a72a18f
132yse <1533540012@qq.com>
2019-10-13 17:55:25 +0800

    搞定slieder

--- src/widget/slider/index.js
+++ src/widget/slider/index.js
@@ -12,22 +12,22 @@ export default function Slider () {
       node: wrap
     })
   })
+  const arr = [0, 1, 2, 3]
   return (
     <div id='wrap' ref={wrap}>
       <ul class='imgs'>
-        {[0, 1, 2, 3].map((item) => (
+        <li>
+          <img src={`https://cdn.jsdelivr.net/gh/cliclitv/fre-clicli-home@master/src/common/img/sliders/${arr[arr.length - 1]}.jpg?env=dev`} />
+        </li>
+        {arr.map(item => (
           <li>
-            <img
-              src={`https://cdn.jsdelivr.net/gh/cliclitv/fre-clicli-home@master/src/common/img/sliders/${item}.jpg?env=dev`}
-            />
+            <img src={`https://cdn.jsdelivr.net/gh/cliclitv/fre-clicli-home@master/src/common/img/sliders/${item}.jpg?env=dev`} />
           </li>
         ))}
+        <li>
+          <img src={`https://cdn.jsdelivr.net/gh/cliclitv/fre-clicli-home@master/src/common/img/sliders/${arr[0]}.jpg?env=dev`} />
+        </li>
       </ul>
-      <div class='dots'>
-        <b class='active' />
-        <b />
-        <b />
-      </div>
       <div class='prev' />
       <div class='next' />
     </div>
