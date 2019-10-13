35f25d1fed0842f9af817d2fd8cea2ef28718b3a
132yse <1533540012@qq.com>
2019-10-13 20:51:31 +0800

    搞定标签

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -1,6 +1,7 @@
 import { h } from 'fre'
 import Slider from '../../widget/slider'
 import Header from '../header'
+import Tag from '../../widget/tag'
 import './index.styl'
 
 export default function Home () {
@@ -8,6 +9,7 @@ export default function Home () {
     <div>
       <Slider />
       <Header />
+      <Tag />
     </div>
   )
 }
---
+++ src/widget/tag/index.js
@@ -0,0 +1,32 @@
+import { h } from 'fre'
+import './index.styl'
+
+
+export default function Tag () {
+  const arr = [
+    '耽美',
+    '乙女',
+    '百合',
+    '后宫',
+    '热血',
+    '运动',
+    '冒险',
+    '战斗',
+    'r15',
+    '泡面番',
+    '治愈',
+    '推理',
+    '特摄',
+    '真人剧'
+  ]
+  return (
+    <div class='tag'>
+      <div class='wrap'>
+        {arr.map(item => (
+          <a>{item}</a>
+        ))}
+        <a href="">更多></a>
+      </div>
+    </div>
+  )
+}
