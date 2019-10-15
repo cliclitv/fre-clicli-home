5aaacf8d446ab9fc331d4b0024899cd4344d98a3
132yse <1533540012@qq.com>
2019-10-15 21:28:01 +0800

    搞定底部

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -1,24 +1,32 @@
-import { h } from 'fre'
+import {h} from 'fre'
 import './index.styl'
 
 export default function Footer() {
-  return(
+  return (
     <div className="footer">
-      <div className="title">
-        <img src="https://pic.superbed.cn/item/5cc52b543a213b04172979c8.png"/>
-        <h1>CliCli弹幕网</h1>
+      <div className="wrap">
+        <div className="title">
+          <img src="https://pic.superbed.cn/item/5cc52b543a213b04172979c8.png"/>
+          <h1>CliCli 弹幕网</h1>
+        </div>
+        <ul className="links">
+          <li><a href="">关于我们</a></li>
+          <li><a href="">联系我们</a></li>
+          <li><a href="">up主招募</a></li>
+          <li><a href="">友情链接</a></li>
+        </ul>
+        <ul className="icons">
+          <a href="">
+            <li><i className="icon-font icon-qq"></i>QQ群</li>
+          </a>
+          <a href="">
+            <li><i className="icon-font icon-weibo"></i>微博</li>
+          </a>
+          <a href="">
+            <li><i className="icon-font icon-xiazai"></i>下载客户端</li>
+          </a>
+        </ul>
       </div>
-      <ul className="links">
-        <li><a href="">关于我们</a></li>
-        <li><a href="">联系我们</a></li>
-        <li><a href="">up主招募</a></li>
-        <li><a href="">友情链接</a></li>
-      </ul>
-      <ul className="icons">
-        <li><a href=""><i className="icon-font icon-qq"></i>QQ群</a></li>
-        <li><a href=""><i className="icon-font icon-weibo"></i>微博</a></li>
-        <li><a href=""><i className="icon-font icon-xiazai"></i>下载客户端</a></li>
-      </ul>
     </div>
   )
 }
