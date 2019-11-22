e64ec64b117cdeea075f7c5398c731c0841f9d43
132yse <1533540012@qq.com>
2019-11-22 14:01:40 +0800

    搞定新番表索引

--- src/component/week-list/index.js
+++ src/component/week-list/index.js
@@ -5,36 +5,33 @@ import './index.styl'
 
 export default function Recommend(props) {
   const [posts, setPosts] = useState([])
+  const [day,setDay] = useState(new Date().getDay())
   useEffect(() => {
-    getPost('nowait', '新番', 1, 100).then(res => {
-      let ret = {
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        0: [],
-      }
-      res.data.posts.forEach(item => {
+    getPost('新番', '', 1, 100).then(res => {
+      let ret = {}
+      res.posts.forEach(item => {
         let day = new Date(item.time).getDay()
+        ret[day] = ret[day] || []
         ret[day].push(item)
       })
       setPosts(ret)
-      window.ret = ret
     })
   }, [])
-  return <div className="recommend">
-    <h1>更新表</h1>
-    <ul>
-      {posts.map(item => {
-        return <li>
-          <div className="cover">
-            <img src={getSuo(item.content)}/>
-          </div>
-          <div className="title">{item.title}</div>
-        </li>
-      })}
-    </ul>
+  const map = {
+    0: '周日',
+    1: '周一',
+    2: '周二',
+    3: '周三',
+    4: '周四',
+    5: '周五',
+    6: '周六'
+  }
+  return <div className="week-list">
+    <div className="wrap">
+      <h1>更新表</h1>
+      <ul>
+        {Object.keys(posts).map((item,index) => <button className={index===day?'active':''} onClick={()=>setDay(index)}>{map[item]}</button>)}
+      </ul>
+    </div>
   </div>
 }
