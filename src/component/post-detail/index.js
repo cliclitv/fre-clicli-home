eb8f29ffe3204ffc3292ea764a7960733dc5e851
132yse <1533540012@qq.com>
2019-12-10 11:44:08 +0800

    日常提交

--- src/component/post-detail/index.js
+++ src/component/post-detail/index.js
@@ -20,16 +20,16 @@ export default function PostDetal(props) {
 
   return (<div className="post-detail">
     <div className='left'>
-      <article ref={t}></article>
+      <article ref={t}>少男祈祷中……</article>
     </div>
     <div className="right">
       <div className="info">
         <div className="user">
           <span className='avatar'><img src={getAvatar(post.uqq)} alt={post.uqq}></img></span>
-          <span className='uname'>{post.uname}</span>
-          <span className='uid'>uid：{post.uid||''}</span>
+          <span className='uname'>{post.uname||''}</span>
+          <span className='uid'>uid：{post.uid||0}</span>
         </div>
-        <h1>{post.title||''}</h1>
+        <h1>{post.title||'少男祈祷中……'}</h1>
         <div>
           <span>{post.tag||''}</span>
           <span>{post.time||''}</span>
