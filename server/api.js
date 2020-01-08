e28af7736700fb35fedd553a5c32dd291ce63e56
132yse <1533540012@qq.com>
2020-01-08 10:46:18 +0800

    修改端口

--- server/api.js
+++ server/api.js
@@ -75,6 +75,6 @@ app.use(async (ctx, next) => {
 
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(5000, '0.0.0.0', () => {
+app.listen(8082, '0.0.0.0', () => {
   console.log('api started on 5000')
 })
