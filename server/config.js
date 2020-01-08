691f9d0c689a83fbb527a7ed5b04b0552f9043a0
132yse <1533540012@qq.com>
2020-01-08 10:31:10 +0800

    日常提交

---
+++ server/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: 'Zchanghao1234',
+      database: 'clicli'
+    }
+  }
+}
